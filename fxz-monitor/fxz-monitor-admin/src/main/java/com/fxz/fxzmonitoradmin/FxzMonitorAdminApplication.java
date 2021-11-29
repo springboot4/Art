f4b07b202e17265d06588cf74d4497303d3f83ac
付绪壮 <2235602974@qq.com>
2021-11-29 13:58:07 +0800

    整合springboot admin

---
+++ fxz-monitor/fxz-monitor-admin/src/main/java/com/fxz/fxzmonitoradmin/FxzMonitorAdminApplication.java
@@ -0,0 +1,18 @@
+package com.fxz.fxzmonitoradmin;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author fxz
+ */
+@EnableAdminServer
+@SpringBootApplication
+public class FxzMonitorAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FxzMonitorAdminApplication.class, args);
+    }
+
+}
