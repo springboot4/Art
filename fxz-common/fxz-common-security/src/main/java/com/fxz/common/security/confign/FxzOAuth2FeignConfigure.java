7320676bd01a5f279531f6ee822f1e728f114dd1
付绪壮 <2235602974@qq.com>
2022-02-27 09:18:06 +0800

    :memo: 为feign请求头添加令牌

--- fxz-common/fxz-common-security/src/main/java/com/fxz/common/security/confign/FxzOAuth2FeignConfigure.java
+++ fxz-common/fxz-common-security/src/main/java/com/fxz/common/security/confign/FxzOAuth2FeignConfigure.java
@@ -21,9 +21,9 @@ public class FxzOAuth2FeignConfigure {
 	@Bean
 	public RequestInterceptor oauth2FeignRequestInterceptor() {
 		return requestTemplate -> {
-			// 添加 Zuul Token
-			String zuulToken = new String(Base64Utils.encode(FxzConstant.ZUUL_TOKEN_VALUE.getBytes()));
-			requestTemplate.header(FxzConstant.ZUUL_TOKEN_HEADER, zuulToken);
+			// 添加 Gateway Token
+			String gatewayToken = new String(Base64Utils.encode(FxzConstant.GATEWAY_TOKEN_VALUE.getBytes()));
+			requestTemplate.header(FxzConstant.GATEWAY_TOKEN_HEADER, gatewayToken);
 
 			Object details = SecurityContextHolder.getContext().getAuthentication().getDetails();
 			if (details instanceof OAuth2AuthenticationDetails) {
