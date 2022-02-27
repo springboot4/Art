32222320eb7a423d2b5360a83beff6efab7e9eab
付绪壮 <2235602974@qq.com>
2022-02-27 09:14:27 +0800

    :memo: 经过网关的请求头携带GATEWAY Token

--- fxz-gateway/src/main/java/com/fxz/gateway/filter/FxzGatewayRequestFilter.java
+++ fxz-gateway/src/main/java/com/fxz/gateway/filter/FxzGatewayRequestFilter.java
@@ -39,69 +39,69 @@ import static org.springframework.cloud.gateway.support.ServerWebExchangeUtils.*
 @RequiredArgsConstructor
 public class FxzGatewayRequestFilter implements GlobalFilter {
 
-	private final FxzGatewayProperties properties;
+    private final FxzGatewayProperties properties;
 
-	private final AntPathMatcher pathMatcher = new AntPathMatcher();
+    private final AntPathMatcher pathMatcher = new AntPathMatcher();
 
-	@Override
-	public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-		ServerHttpRequest request = exchange.getRequest();
-		ServerHttpResponse response = exchange.getResponse();
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        ServerHttpRequest request = exchange.getRequest();
+        ServerHttpResponse response = exchange.getResponse();
 
-		// 禁止客户端的访问资源逻辑
-		Mono<Void> checkForbidUriResult = checkForbidUri(request, response);
-		if (checkForbidUriResult != null) {
-			return checkForbidUriResult;
-		}
+        // 禁止客户端的访问资源逻辑
+        Mono<Void> checkForbidUriResult = checkForbidUri(request, response);
+        if (checkForbidUriResult != null) {
+            return checkForbidUriResult;
+        }
 
-		// 日志打印
-		printLog(exchange);
+        // 日志打印
+        printLog(exchange);
 
-		byte[] token = Base64Utils.encode((FxzConstant.ZUUL_TOKEN_VALUE).getBytes());
-		ServerHttpRequest build = request.mutate().header(FxzConstant.ZUUL_TOKEN_HEADER, new String(token)).build();
-		ServerWebExchange newExchange = exchange.mutate().request(build).build();
-		return chain.filter(newExchange);
-	}
+        byte[] token = Base64Utils.encode((FxzConstant.GATEWAY_TOKEN_VALUE).getBytes());
+        ServerHttpRequest build = request.mutate().header(FxzConstant.GATEWAY_TOKEN_HEADER, new String(token)).build();
+        ServerWebExchange newExchange = exchange.mutate().request(build).build();
+        return chain.filter(newExchange);
+    }
 
-	private Mono<Void> checkForbidUri(ServerHttpRequest request, ServerHttpResponse response) {
-		String uri = request.getPath().toString();
-		boolean shouldForward = true;
-		String forbidRequestUri = properties.getForbidRequestUri();
-		String[] forbidRequestUris = StringUtils.splitByWholeSeparatorPreserveAllTokens(forbidRequestUri, ",");
-		if (ArrayUtils.isNotEmpty(forbidRequestUris)) {
-			for (String u : forbidRequestUris) {
-				if (pathMatcher.match(u, uri)) {
-					shouldForward = false;
-				}
-			}
-		}
-		if (!shouldForward) {
-			FxzResponse fxzResponse = new FxzResponse().message("该URI不允许外部访问");
-			return makeResponse(response, fxzResponse);
-		}
-		return null;
-	}
+    private Mono<Void> checkForbidUri(ServerHttpRequest request, ServerHttpResponse response) {
+        String uri = request.getPath().toString();
+        boolean shouldForward = true;
+        String forbidRequestUri = properties.getForbidRequestUri();
+        String[] forbidRequestUris = StringUtils.splitByWholeSeparatorPreserveAllTokens(forbidRequestUri, ",");
+        if (ArrayUtils.isNotEmpty(forbidRequestUris)) {
+            for (String u : forbidRequestUris) {
+                if (pathMatcher.match(u, uri)) {
+                    shouldForward = false;
+                }
+            }
+        }
+        if (!shouldForward) {
+            FxzResponse fxzResponse = new FxzResponse().message("该URI不允许外部访问");
+            return makeResponse(response, fxzResponse);
+        }
+        return null;
+    }
 
-	private Mono<Void> makeResponse(ServerHttpResponse response, FxzResponse fxzResponse) {
-		response.setStatusCode(HttpStatus.FORBIDDEN);
-		response.getHeaders().add(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
-		DataBuffer dataBuffer = response.bufferFactory().wrap(JSONObject.toJSONString(fxzResponse).getBytes());
-		return response.writeWith(Mono.just(dataBuffer));
-	}
+    private Mono<Void> makeResponse(ServerHttpResponse response, FxzResponse fxzResponse) {
+        response.setStatusCode(HttpStatus.FORBIDDEN);
+        response.getHeaders().add(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
+        DataBuffer dataBuffer = response.bufferFactory().wrap(JSONObject.toJSONString(fxzResponse).getBytes());
+        return response.writeWith(Mono.just(dataBuffer));
+    }
 
-	private void printLog(ServerWebExchange exchange) {
-		URI url = exchange.getAttribute(GATEWAY_REQUEST_URL_ATTR);
-		Route route = exchange.getAttribute(GATEWAY_ROUTE_ATTR);
-		LinkedHashSet<URI> uris = exchange.getAttribute(GATEWAY_ORIGINAL_REQUEST_URL_ATTR);
-		URI originUri = null;
-		if (uris != null) {
-			originUri = uris.stream().findFirst().orElse(null);
-		}
-		if (url != null && route != null && originUri != null) {
-			log.info("转发请求：{}://{}{} --> 目标服务：{}，目标地址：{}://{}{}，转发时间：{}", originUri.getScheme(),
-					originUri.getAuthority(), originUri.getPath(), route.getId(), url.getScheme(), url.getAuthority(),
-					url.getPath(), LocalDateTime.now());
-		}
-	}
+    private void printLog(ServerWebExchange exchange) {
+        URI url = exchange.getAttribute(GATEWAY_REQUEST_URL_ATTR);
+        Route route = exchange.getAttribute(GATEWAY_ROUTE_ATTR);
+        LinkedHashSet<URI> uris = exchange.getAttribute(GATEWAY_ORIGINAL_REQUEST_URL_ATTR);
+        URI originUri = null;
+        if (uris != null) {
+            originUri = uris.stream().findFirst().orElse(null);
+        }
+        if (url != null && route != null && originUri != null) {
+            log.info("转发请求：{}://{}{} --> 目标服务：{}，目标地址：{}://{}{}，转发时间：{}", originUri.getScheme(),
+                    originUri.getAuthority(), originUri.getPath(), route.getId(), url.getScheme(), url.getAuthority(),
+                    url.getPath(), LocalDateTime.now());
+        }
+    }
 
 }
