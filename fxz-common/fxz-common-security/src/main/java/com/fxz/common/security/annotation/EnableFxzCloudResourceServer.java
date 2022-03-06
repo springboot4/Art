405b57f3e2db0a6f59fc64862b735cd40e114ad0
付绪壮 <2235602974@qq.com>
2022-03-06 20:25:05 +0800

    :fire: 删除EnableFxzAuth2FeignConfigure注解，为feign请求头添加令牌在FxzCloudSecurityAutoConfigure自动装配类中实现

--- fxz-common/fxz-common-security/src/main/java/com/fxz/common/security/annotation/EnableFxzCloudResourceServer.java
+++ fxz-common/fxz-common-security/src/main/java/com/fxz/common/security/annotation/EnableFxzCloudResourceServer.java
@@ -7,11 +7,14 @@ import java.lang.annotation.*;
 
 /**
  * @author fxz
+ * 资源服务器注解，通过添加此注解实现配置ResourceServerConfigurerAdapter的效果
+ * 资源服务器需要忽略鉴权在配置文件中添加fxz:cloud:security:anonUris
+ * 具体配置参考:
+ * @see com.fxz.common.security.properties.FxzCloudSecurityProperties
  */
-@Target({ ElementType.TYPE })
+@Target({ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
 @Import(FxzCloudResourceServerConfigure.class)
 public @interface EnableFxzCloudResourceServer {
-
 }
