63f7b9bee24a141725a286debd381d1461ced357
付绪壮 <2235602974@qq.com>
2022-04-04 23:51:30 +0800

    :tada: 文件管理

---
+++ fxz-common/fxz-common-file/src/main/java/com/fxz/common/file/OssProperties.java
@@ -0,0 +1,20 @@
+package com.fxz.common.file;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@Data
+@ConfigurationProperties(prefix = "oss")
+public class OssProperties {
+
+	private String endpoint;
+
+	private Boolean pathStyleAccess = true;
+
+	private String accessKey;
+
+	private String secretKey;
+
+	private String bucketName = "fxzCloud";
+
+}
---
+++ fxz-server/fxz-server-system/fxz-server-system-biz/src/main/java/com/fxz/system/controller/FileController.java
@@ -0,0 +1,88 @@
+package com.fxz.system.controller;
+
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.fxz.common.mp.result.PageResult;
+import com.fxz.common.mp.result.Result;
+import com.fxz.system.dto.FileDto;
+import com.fxz.system.entity.File;
+import com.fxz.system.service.FileService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletResponse;
+import java.util.List;
+
+/**
+ * 文件管理
+ *
+ * @author fxz
+ * @date 2022-04-04
+ */
+@RestController
+@RequestMapping("/file")
+@RequiredArgsConstructor
+public class FileController {
+
+	private final FileService fileService;
+
+	/**
+	 * 上传文件
+	 */
+	@PostMapping(value = "/add")
+	public Result<Boolean> add(@RequestPart("file") MultipartFile file) {
+		return Result.success(fileService.addFile(file));
+	}
+
+	/**
+	 * 下载文件
+	 * @param bucket 桶名称
+	 * @param fileName 文件名
+	 * @param response 响应
+	 */
+	@GetMapping("/{bucket}/{fileName}")
+	public void file(@PathVariable String bucket, @PathVariable String fileName, HttpServletResponse response) {
+		fileService.getFile(bucket, fileName, response);
+	}
+
+	/**
+	 * 修改
+	 */
+	@PostMapping(value = "/update")
+	public Result<Boolean> update(@RequestBody FileDto fileDto) {
+		return Result.success(fileService.updateFile(fileDto));
+	}
+
+	/**
+	 * 删除
+	 */
+	@DeleteMapping(value = "/delete")
+	public Result<Boolean> delete(Long id) {
+		return Result.judge(fileService.deleteFile(id));
+	}
+
+	/**
+	 * 获取单条
+	 */
+	@GetMapping(value = "/findById")
+	public Result<File> findById(Long id) {
+		return Result.success(fileService.findById(id));
+	}
+
+	/**
+	 * 获取全部
+	 */
+	@GetMapping(value = "/findAll")
+	public Result<List<File>> findAll() {
+		return Result.success(fileService.findAll());
+	}
+
+	/**
+	 * 分页
+	 */
+	@GetMapping(value = "/page")
+	public Result<PageResult<File>> pageFile(Page<File> pageParam, File file) {
+		return Result.success(PageResult.success(fileService.pageFile(pageParam, file)));
+	}
+
+}
