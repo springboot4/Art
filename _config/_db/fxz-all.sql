7c5a1f6c01528da1aa2e04bb90086bc960786df3
Fxz <2235602974@qq.com>
2022-04-23 23:02:22 +0800

    :card_file_box: 更新sql文件

---
+++ _config/_db/fxz-all.sql
@@ -0,0 +1,7 @@
+create database `fxz_cloud_base` default character set utf8mb4 collate utf8mb4_general_ci;
+
+create database `fxz_cloud_codegen` default character set utf8mb4 collate utf8mb4_general_ci;
+
+create database `fxz_cloud_job` default character set utf8mb4 collate utf8mb4_general_ci;
+
+create database `fxz_cloud_nacos` default character set utf8mb4 collate utf8mb4_general_ci;
