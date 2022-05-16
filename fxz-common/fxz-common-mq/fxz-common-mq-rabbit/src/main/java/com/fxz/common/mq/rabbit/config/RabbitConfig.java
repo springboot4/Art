80865045290363ce878aa027bfaa460fc2fe952c
Fxz <2235602974@qq.com>
2022-05-16 14:40:59 +0800

    :sparkles: common-rabbitmq模块创建，支持动态创建、绑定队列，交换机

---
+++ fxz-common/fxz-common-mq/fxz-common-mq-rabbit/src/main/java/com/fxz/common/mq/rabbit/config/RabbitConfig.java
@@ -0,0 +1,44 @@
+package com.fxz.common.mq.rabbit.config;
+
+import com.fxz.common.mq.rabbit.dynamic.RabbitModuleInitializer;
+import com.fxz.common.mq.rabbit.dynamic.RabbitModuleProperties;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.AmqpAdmin;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.listener.RabbitListenerContainerFactory;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author fxz
+ */
+@Configuration
+@Slf4j
+public class RabbitConfig {
+
+	/**
+	 * 消息序列化配置
+	 * @param connectionFactory
+	 */
+	@Bean
+	public RabbitListenerContainerFactory<?> rabbitListenerContainerFactory(ConnectionFactory connectionFactory) {
+		SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+		factory.setConnectionFactory(connectionFactory);
+		factory.setMessageConverter(new Jackson2JsonMessageConverter());
+		return factory;
+	}
+
+	/*
+	 * 动态创建队列、交换机初始化器
+	 */
+	@Bean
+	@ConditionalOnMissingBean
+	public RabbitModuleInitializer rabbitModuleInitializer(AmqpAdmin amqpAdmin,
+			RabbitModuleProperties rabbitModuleProperties) {
+		return new RabbitModuleInitializer(amqpAdmin, rabbitModuleProperties);
+	}
+
+}
---
+++ fxz-common/fxz-common-mq/fxz-common-mq-rabbit/src/main/java/com/fxz/common/mq/rabbit/dynamic/RabbitModuleProperties.java
@@ -0,0 +1,17 @@
+package com.fxz.common.mq.rabbit.dynamic;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import java.util.List;
+
+/**
+ * @author fxz
+ */
+@ConfigurationProperties(prefix = "spring.rabbitmq")
+@Data
+public class RabbitModuleProperties {
+
+	private List<RabbitModuleInfo> modules;
+
+}
