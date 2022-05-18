cf5a042f8b1ddd27d390ec8e67ad4ccd7f1e1b3e
Fxz <2235602974@qq.com>
2022-05-18 22:14:39 +0800

    :recycle: 修复已知问题

--- fxz-common/fxz-common-core/src/main/java/com/fxz/common/core/exception/FxzException.java
+++ fxz-common/fxz-common-core/src/main/java/com/fxz/common/core/exception/FxzException.java
@@ -5,7 +5,7 @@ package com.fxz.common.core.exception;
  * @version 1.0
  * @date 2021-11-28 19:27
  */
-public class FxzException extends Exception {
+public class FxzException extends RuntimeException {
 
 	private static final long serialVersionUID = -6916154462432027437L;
 
