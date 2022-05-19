e43cd331d984690cb05dddb0572e040b6ff73241
Fxz <2235602974@qq.com>
2022-05-19 21:23:11 +0800

    :white_check_mark: 阿里云短信发送

---
+++ fxz-server/fxz-server-system/fxz-server-system-biz/src/main/java/com/fxz/system/controller/SmsController.java
@@ -0,0 +1,32 @@
+package com.fxz.system.controller;
+
+import com.fxz.common.mp.result.Result;
+import com.fxz.common.security.annotation.Ojbk;
+import com.fxz.common.sms.service.AliyunSmsService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Fxz
+ * @version 1.0
+ * @date 2022/5/19 21:04
+ */
+@Slf4j
+@RestController
+@RequestMapping("/sms")
+@RequiredArgsConstructor
+public class SmsController {
+
+	private final AliyunSmsService aliyunSmsService;
+
+	@Ojbk
+	@GetMapping("/sendSms")
+	public <T> Result<T> sendSmsCode(String phoneNumber) {
+		return Result.judge(aliyunSmsService.sendSmsCode("19806082431"));
+	}
+
+}
