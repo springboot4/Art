95968d9f11f2bac6b60873e4b7b3b2b3c3fb1ca6
Fxz <2235602974@qq.com>
2022-05-28 23:46:57 +0800

    :test_tube: laboratory add es test

--- fxz-scheduled/src/main/java/com/fxz/job/entity/JobLog.java
+++ fxz-scheduled/src/main/java/com/fxz/job/entity/JobLog.java
@@ -22,60 +22,60 @@ import java.util.Date;
 @Accessors(chain = true)
 public class JobLog implements Serializable {
 
-    private static final Long serialVersionUID = -1L;
+	private static final Long serialVersionUID = -1L;
 
-    /**
-     * ID
-     */
-    @TableId
-    private Long jobLogId;
+	/**
+	 * ID
+	 */
+	@TableId
+	private Long jobLogId;
 
-    /**
-     * 任务名称
-     */
-    private String jobName;
+	/**
+	 * 任务名称
+	 */
+	private String jobName;
 
-    /**
-     * 任务组名
-     */
-    private String jobGroup;
+	/**
+	 * 任务组名
+	 */
+	private String jobGroup;
 
-    /**
-     * 调用目标字符串
-     */
-    private String invokeTarget;
+	/**
+	 * 调用目标字符串
+	 */
+	private String invokeTarget;
 
-    /**
-     * 日志信息
-     */
-    private String jobMessage;
+	/**
+	 * 日志信息
+	 */
+	private String jobMessage;
 
-    /**
-     * 执行状态（0正常 1失败）
-     */
-    private String status;
+	/**
+	 * 执行状态（0正常 1失败）
+	 */
+	private String status;
 
-    /**
-     * 异常信息
-     */
-    private String exceptionInfo;
+	/**
+	 * 异常信息
+	 */
+	private String exceptionInfo;
 
-    /**
-     * 创建时间
-     */
-    @TableField(fill = FieldFill.INSERT)
-    private LocalDateTime createTime;
+	/**
+	 * 创建时间
+	 */
+	@TableField(fill = FieldFill.INSERT)
+	private LocalDateTime createTime;
 
-    /**
-     * 开始时间
-     */
-    @TableField(exist = false)
-    private Date startTime;
+	/**
+	 * 开始时间
+	 */
+	@TableField(exist = false)
+	private Date startTime;
 
-    /**
-     * 停止时间
-     */
-    @TableField(exist = false)
-    private Date stopTime;
+	/**
+	 * 停止时间
+	 */
+	@TableField(exist = false)
+	private Date stopTime;
 
 }
