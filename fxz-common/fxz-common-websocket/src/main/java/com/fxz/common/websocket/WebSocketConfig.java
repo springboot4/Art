e94960c33fb2e13761f402e8ba66a005fc0791e0
Fxz <2235602974@qq.com>
2022-07-07 11:34:11 +0800

    :recycle: 替换Configuration为AutoConfiguration

--- fxz-common/fxz-common-websocket/src/main/java/com/fxz/common/websocket/WebSocketConfig.java
+++ fxz-common/fxz-common-websocket/src/main/java/com/fxz/common/websocket/WebSocketConfig.java
@@ -3,10 +3,10 @@ package com.fxz.common.websocket;
 import com.fxz.common.websocket.handler.UserNoticeWebSocketHandler;
 import com.fxz.common.websocket.interceptor.UserNoticeWebSocketInterceptor;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
@@ -17,7 +17,7 @@ import org.springframework.web.socket.server.standard.ServerEndpointExporter;
  * @version 1.0
  * @date 2022/6/28 09:23
  */
-@Configuration
+@AutoConfiguration
 @ComponentScan
 @EnableWebSocket
 @RequiredArgsConstructor
