f9c932b6721602e624276c0981f5d8ace1b04111
付绪壮 <2235602974@qq.com>
2022-08-19 21:09:35 +0800

    :zap: 网关代码优化

--- fxz-gateway/src/main/java/com/fxz/gateway/filter/FxzGatewayRequestFilter.java
+++ fxz-gateway/src/main/java/com/fxz/gateway/filter/FxzGatewayRequestFilter.java
@@ -11,6 +11,7 @@ import org.apache.commons.lang3.StringUtils;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.cloud.gateway.route.Route;
+import org.springframework.core.Ordered;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpResponse;
@@ -27,7 +28,7 @@ import java.util.LinkedHashSet;
 import static org.springframework.cloud.gateway.support.ServerWebExchangeUtils.*;
 
 /**
- * 黑名单 不允许访问的地址
+ * 日志、黑名单过滤器
  *
  * @author Fxz
  * @version 1.0
@@ -36,11 +37,13 @@ import static org.springframework.cloud.gateway.support.ServerWebExchangeUtils.*
 @Slf4j
 @Component
 @RequiredArgsConstructor
-public class FxzGatewayRequestFilter implements GlobalFilter {
+public class FxzGatewayRequestFilter implements GlobalFilter, Ordered {
 
 	private final FxzGatewayProperties properties;
 
 	private final AntPathMatcher pathMatcher = new AntPathMatcher();
+
+	private final String COUNT_START_TIME = "countStartTime";
 
 	@Override
 	public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
@@ -53,26 +56,35 @@ public class FxzGatewayRequestFilter implements GlobalFilter {
 			return checkForbidUriResult;
 		}
 
-		// 日志打印
-		printLog(exchange);
+		// 方法计时
+		exchange.getAttributes().put(COUNT_START_TIME, System.currentTimeMillis());
 
+		// 标记请求来自网关
 		byte[] token = Base64Utils.encode((FxzConstant.GATEWAY_TOKEN_VALUE).getBytes());
 		ServerHttpRequest build = request.mutate().header(FxzConstant.GATEWAY_TOKEN_HEADER, new String(token)).build();
-		return chain.filter(exchange.mutate().request(build).build());
+
+		// then方法相当于aop的后置通知一样
+		return chain.filter(exchange.mutate().request(build).build()).then(Mono.fromRunnable(() -> printLog(exchange)));
 	}
 
+	/**
+	 * 检验url是否禁止访问
+	 */
 	private Mono<Void> checkForbidUri(ServerHttpRequest request, ServerHttpResponse response) {
 		String uri = request.getPath().toString();
-		boolean shouldForward = true;
 		String forbidRequestUri = properties.getForbidRequestUri();
 		String[] forbidRequestUris = StringUtils.splitByWholeSeparatorPreserveAllTokens(forbidRequestUri, ",");
+		boolean shouldForward = true;
+
 		if (ArrayUtils.isNotEmpty(forbidRequestUris)) {
 			for (String u : forbidRequestUris) {
 				if (pathMatcher.match(u, uri)) {
 					shouldForward = false;
+					break;
 				}
 			}
 		}
+
 		if (!shouldForward) {
 			return WebFluxUtil.webFluxResponseWriter(response, Result.failed("该URI不允许外部访问"),
 					HttpStatus.FORBIDDEN.value());
@@ -80,19 +92,35 @@ public class FxzGatewayRequestFilter implements GlobalFilter {
 		return null;
 	}
 
+	/**
+	 * 打印执行日志
+	 */
 	private void printLog(ServerWebExchange exchange) {
 		URI url = exchange.getAttribute(GATEWAY_REQUEST_URL_ATTR);
 		Route route = exchange.getAttribute(GATEWAY_ROUTE_ATTR);
 		LinkedHashSet<URI> uris = exchange.getAttribute(GATEWAY_ORIGINAL_REQUEST_URL_ATTR);
+
+		Long startTime = exchange.getAttribute(COUNT_START_TIME);
+		Long executeTime = (System.currentTimeMillis() - startTime);
+
 		URI originUri = null;
 		if (uris != null) {
 			originUri = uris.stream().findFirst().orElse(null);
 		}
+
 		if (url != null && route != null && originUri != null) {
-			log.info("转发请求：{}://{}{} --> 目标服务：{}，目标地址：{}://{}{}，转发时间：{}", originUri.getScheme(),
+			log.info("转发请求：{}://{}{} --> 目标服务：{}，目标地址：{}://{}{}，转发时间：{}，方法耗时：{}", originUri.getScheme(),
 					originUri.getAuthority(), originUri.getPath(), route.getId(), url.getScheme(), url.getAuthority(),
-					url.getPath(), LocalDateTime.now());
+					url.getPath(), LocalDateTime.now(), executeTime);
 		}
+	}
+
+	/**
+	 * 数值越低，过滤器优先级越高
+	 */
+	@Override
+	public int getOrder() {
+		return Ordered.HIGHEST_PRECEDENCE;
 	}
 
 }
