ab0d35b9e23120b76ebe89db5ac3f8bfbe53de97
付绪壮 <2235602974@qq.com>
2022-09-03 19:08:33 +0800

    :white_check_mark: redis实现分布式锁demo

---
+++ fxz-z-demos/src/main/java/com/fxz/demos/distributed/lock/DistributedLockClient.java
@@ -0,0 +1,30 @@
+package com.fxz.demos.distributed.lock;
+
+import com.fxz.demos.distributed.lock.redis.DistributedRedisLock;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+/**
+ * @author Fxz
+ * @version 1.0
+ * @date 2022/9/3 15:33
+ */
+@Component
+@RequiredArgsConstructor
+public class DistributedLockClient {
+
+	private final StringRedisTemplate redisTemplate;
+
+	/**
+	 * DistributedLockClient为单例 uuid在每个服务下唯一
+	 */
+	private final String uuid = UUID.randomUUID().toString();
+
+	public DistributedRedisLock getRedisLock(String lockName) {
+		return new DistributedRedisLock(redisTemplate, lockName, uuid);
+	}
+
+}
