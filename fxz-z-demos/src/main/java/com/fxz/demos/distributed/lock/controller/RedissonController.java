e82c1f5e41acce41b0394bc53c2565529645b9f6
付绪壮 <2235602974@qq.com>
2022-09-05 00:54:11 +0800

    :sparkles: 支持注解声明式分布式锁

--- fxz-z-demos/src/main/java/com/fxz/demos/distributed/lock/controller/RedissonController.java
+++ fxz-z-demos/src/main/java/com/fxz/demos/distributed/lock/controller/RedissonController.java
@@ -1,8 +1,10 @@
 package com.fxz.demos.distributed.lock.controller;
 
+import com.fxz.common.lock.annotation.DistributedLock;
 import com.fxz.common.lock.constant.RedissonLockType;
 import com.fxz.common.lock.entity.LockEntity;
 import com.fxz.common.lock.factory.RedissonLockServiceFactory;
+import com.fxz.common.lock.lockresolver.impl.ExpressionDistributedLockKeyResolver;
 import com.fxz.common.lock.service.RedissonService;
 import com.fxz.common.mp.result.Result;
 import com.fxz.common.security.annotation.Ojbk;
@@ -32,6 +34,77 @@ public class RedissonController {
 	private final RedissonService readService = RedissonLockServiceFactory.getLock(RedissonLockType.READ);
 
 	private final RedissonService writeService = RedissonLockServiceFactory.getLock(RedissonLockType.WRITE);
+
+	/**
+	 * 测试声明式加锁
+	 */
+	@DistributedLock(lockName = "reentrant", lockType = RedissonLockType.REENTRANT)
+	@Ojbk
+	@SneakyThrows
+	@GetMapping("/reentrantAnn")
+	public Result<String> reentrantAnn() {
+		log.info("抢到可重入锁：{}", Thread.currentThread().getId());
+		TimeUnit.SECONDS.sleep(5);
+		return Result.success(LocalDateTime.now().toString());
+	}
+
+	/**
+	 * 测试声明式加锁 不指定锁名称
+	 */
+	@DistributedLock(lockType = RedissonLockType.REENTRANT)
+	@Ojbk
+	@SneakyThrows
+	@GetMapping("/reentrantAnnNo")
+	public Result<String> reentrantAnnNo() {
+		log.info("抢到可重入锁：{}", Thread.currentThread().getId());
+		TimeUnit.SECONDS.sleep(5);
+		return Result.success(LocalDateTime.now().toString());
+	}
+
+	/**
+	 * 测试声明式加锁 spel
+	 */
+	@DistributedLock(lockName = "#name", lockType = RedissonLockType.REENTRANT,
+			lockResolver = ExpressionDistributedLockKeyResolver.class)
+	@Ojbk
+	@SneakyThrows
+	@GetMapping("/reentrantAnnSpel")
+	public Result<String> reentrantAnnSpel(String name) {
+		log.info("抢到可重入锁：{}", Thread.currentThread().getId());
+		log.info("name:{}", name);
+		TimeUnit.SECONDS.sleep(5);
+		return Result.success(LocalDateTime.now().toString());
+	}
+
+	/**
+	 * 测试声明式加锁 读锁
+	 */
+	@DistributedLock(lockName = "#name", lockType = RedissonLockType.READ,
+			lockResolver = ExpressionDistributedLockKeyResolver.class)
+	@Ojbk
+	@SneakyThrows
+	@GetMapping("/readAnn")
+	public Result<String> readAnn(String name) {
+		log.info("抢到读锁：{}", Thread.currentThread().getId());
+		log.info("name:{}", name);
+		TimeUnit.SECONDS.sleep(5);
+		return Result.success(LocalDateTime.now().toString());
+	}
+
+	/**
+	 * 测试声明式加锁 写锁
+	 */
+	@DistributedLock(lockName = "#name", lockType = RedissonLockType.WRITE,
+			lockResolver = ExpressionDistributedLockKeyResolver.class)
+	@Ojbk
+	@SneakyThrows
+	@GetMapping("/writeAnn")
+	public Result<String> writeAnn(String name) {
+		log.info("抢到写读：{}", Thread.currentThread().getId());
+		log.info("name:{}", name);
+		TimeUnit.SECONDS.sleep(5);
+		return Result.success(LocalDateTime.now().toString());
+	}
 
 	@Ojbk
 	@SneakyThrows
