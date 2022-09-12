d5f63ea9104b214d0f0beedfcbefc2507fb5053c
付绪壮 <2235602974@qq.com>
2022-09-12 16:44:07 +0800

    :sparkles: 应用管理

--- fxz-system/fxz-system-api/src/main/java/com/fxz/system/entity/Menu.java
+++ fxz-system/fxz-system-api/src/main/java/com/fxz/system/entity/Menu.java
@@ -6,6 +6,7 @@ import com.baomidou.mybatisplus.annotation.TableName;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fxz.common.mp.base.BaseEntity;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import java.io.Serializable;
 
@@ -16,6 +17,7 @@ import java.io.Serializable;
  */
 @JsonInclude(value = JsonInclude.Include.NON_NULL)
 @Data
+@EqualsAndHashCode(callSuper = true)
 @TableName("sys_menu")
 public class Menu extends BaseEntity implements Serializable {
 
