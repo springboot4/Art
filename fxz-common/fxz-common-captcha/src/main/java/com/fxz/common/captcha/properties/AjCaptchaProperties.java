b9f22e4f6779e8a4079862658644c0836017c8b6
付绪壮 <2235602974@qq.com>
2022-09-19 21:35:54 +0800

    :zap: 完善代码

--- fxz-common/fxz-common-captcha/src/main/java/com/fxz/common/captcha/properties/AjCaptchaProperties.java
+++ fxz-common/fxz-common-captcha/src/main/java/com/fxz/common/captcha/properties/AjCaptchaProperties.java
@@ -73,7 +73,7 @@ public class AjCaptchaProperties {
 	private String timingClear = "180";
 
 	/**
-	 * 缓存类型redis/local/....
+	 * 缓存类型
 	 */
 	private StorageType cacheType = StorageType.local;
 
@@ -87,23 +87,29 @@ public class AjCaptchaProperties {
 	 */
 	private boolean reqFrequencyLimitEnable = false;
 
-	/***
+	/**
 	 * 一分钟内check接口失败次数
 	 */
 	private int reqGetLockLimit = 5;
 
 	/**
-	 *
+	 * 验证失败后，锁定时间间隔
 	 */
 	private int reqGetLockSeconds = 300;
 
-	/***
+	/**
 	 * get接口一分钟内限制访问数
 	 */
 	private int reqGetMinuteLimit = 100;
 
+	/**
+	 * check接口一分钟内请求数限制
+	 */
 	private int reqCheckMinuteLimit = 100;
 
+	/**
+	 * verify接口一分钟内请求数限制
+	 */
 	private int reqVerifyMinuteLimit = 100;
 
 	/**
