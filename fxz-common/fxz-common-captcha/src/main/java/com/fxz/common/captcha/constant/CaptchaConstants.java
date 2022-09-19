ec6a9107d9d29fabce70440966452edb97e7e09b
付绪壮 <2235602974@qq.com>
2022-09-19 20:53:05 +0800

    :sparkles: 行为验证码代替图片验证码

---
+++ fxz-common/fxz-common-captcha/src/main/java/com/fxz/common/captcha/constant/CaptchaConstants.java
@@ -0,0 +1,12 @@
+package com.fxz.common.captcha.constant;
+
+/**
+ * @author Fxz
+ * @version 1.0
+ * @date 2022/9/19 16:15
+ */
+public interface CaptchaConstants {
+
+	String REDIS = "redis";
+
+}
