33c85b19501ad7bda3ddd9653c5c01ac7b5b482f
付绪壮 <2235602974@qq.com>
2022-11-27 22:15:07 +0800

    :sparkles: 消息队列新增基于redis发布订阅实现

--- art-framework/art-spring-boot-starter-mq/art-spring-boot-starter-mq-redis/src/main/java/com/art/common/mq/redis/core/RedisMQTemplate.java
+++ art-framework/art-spring-boot-starter-mq/art-spring-boot-starter-mq-redis/src/main/java/com/art/common/mq/redis/core/RedisMQTemplate.java
@@ -19,9 +19,11 @@ package com.art.common.mq.redis.core;
 import com.art.common.jackson.util.JacksonUtil;
 import com.art.common.mq.redis.interceptor.RedisMessageInterceptor;
 import com.art.common.mq.redis.message.AbstractRedisMessage;
+import com.art.common.mq.redis.pubsub.AbstractPubSubMessage;
 import com.art.common.mq.redis.stream.AbstractStreamMessage;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.springframework.data.redis.connection.stream.ObjectRecord;
 import org.springframework.data.redis.connection.stream.RecordId;
 import org.springframework.data.redis.connection.stream.StreamRecords;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -30,36 +32,50 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Redis MQ 操作模板类
+ * redis消息队列操作类
  *
  * @author fxz
  */
+@Getter
+@SuppressWarnings("all")
 @AllArgsConstructor
 public class RedisMQTemplate {
 
-	@Getter
 	private final RedisTemplate<String, ?> redisTemplate;
 
 	/**
 	 * 拦截器数组
 	 */
-	@Getter
 	private final List<RedisMessageInterceptor> interceptors = new ArrayList<>();
 
 	/**
-	 * 发送 Redis 消息，基于 Redis Stream 实现
+	 * 基于Stream
 	 * @param message 消息
 	 * @return 消息记录的编号对象
 	 */
 	public <T extends AbstractStreamMessage> RecordId send(T message) {
 		try {
+			// 发送消息前拦截器处理
 			sendMessageBefore(message);
+			// 构建 ObjectRecord
+			ObjectRecord<String, String> record = StreamRecords.newRecord().ofObject(JacksonUtil.toJsonString(message))
+					.withStreamKey(message.getStreamKey());
 			// 发送消息
-			return redisTemplate.opsForStream().add(StreamRecords.newRecord()
-					// 设置内容
-					.ofObject(JacksonUtil.toJsonString(message))
-					// 设置 stream key
-					.withStreamKey(message.getStreamKey()));
+			return redisTemplate.opsForStream().add(record);
+		}
+		finally {
+			// 消息发送后拦截器处理
+			sendMessageAfter(message);
+		}
+	}
+
+	/**
+	 * 基于发布订阅
+	 */
+	public <T extends AbstractPubSubMessage> void send(T message) {
+		try {
+			sendMessageBefore(message);
+			redisTemplate.convertAndSend(message.getTopic(), JacksonUtil.toJsonString(message));
 		}
 		finally {
 			sendMessageAfter(message);
@@ -68,19 +84,16 @@ public class RedisMQTemplate {
 
 	/**
 	 * 添加拦截器
-	 * @param interceptor 拦截器
 	 */
 	public void addInterceptor(RedisMessageInterceptor interceptor) {
 		interceptors.add(interceptor);
 	}
 
 	private void sendMessageBefore(AbstractRedisMessage message) {
-		// 正序
 		interceptors.forEach(interceptor -> interceptor.sendMessageBefore(message));
 	}
 
 	private void sendMessageAfter(AbstractRedisMessage message) {
-		// 倒序
 		for (int i = interceptors.size() - 1; i >= 0; i--) {
 			interceptors.get(i).sendMessageAfter(message);
 		}
