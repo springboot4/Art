0f83a426c8a2e6477c50e8f4ba8ffe78903e9699
付绪壮 <2235602974@qq.com>
2022-12-29 11:21:54 +0800

    :arrow_up: Upgrading dependencies. spring-javaformat-maven-plugin 0.0.35

--- art-gateway/src/main/java/com/art/gateway/component/EasyCaptchaProducer.java
+++ art-gateway/src/main/java/com/art/gateway/component/EasyCaptchaProducer.java
@@ -62,31 +62,31 @@ public class EasyCaptchaProducer {
 	public Captcha getCaptcha(CodeTypeEnum codeType) {
 		Captcha captcha;
 		switch (codeType) {
-		case ARITHMETIC:
-			// 算术类型
-			captcha = new FixedArithmeticCaptcha(width, height);
-			// 固定设置为两位，图片为算数运算表达式
-			captcha.setLen(2);
-			break;
-		case CHINESE:
-			captcha = new ChineseCaptcha(width, height);
-			captcha.setLen(length);
-			break;
-		case CHINESE_GIF:
-			captcha = new ChineseGifCaptcha(width, height);
-			captcha.setLen(length);
-			break;
-		case GIF:
-			// 最后一位是位数
-			captcha = new GifCaptcha(width, height);
-			captcha.setLen(length);
-			break;
-		case SPEC:
-			captcha = new SpecCaptcha(width, height);
-			captcha.setLen(length);
-			break;
-		default:
-			throw new RuntimeException("验证码配置信息错误！");
+			case ARITHMETIC:
+				// 算术类型
+				captcha = new FixedArithmeticCaptcha(width, height);
+				// 固定设置为两位，图片为算数运算表达式
+				captcha.setLen(2);
+				break;
+			case CHINESE:
+				captcha = new ChineseCaptcha(width, height);
+				captcha.setLen(length);
+				break;
+			case CHINESE_GIF:
+				captcha = new ChineseGifCaptcha(width, height);
+				captcha.setLen(length);
+				break;
+			case GIF:
+				// 最后一位是位数
+				captcha = new GifCaptcha(width, height);
+				captcha.setLen(length);
+				break;
+			case SPEC:
+				captcha = new SpecCaptcha(width, height);
+				captcha.setLen(length);
+				break;
+			default:
+				throw new RuntimeException("验证码配置信息错误！");
 		}
 		captcha.setFont(new Font(fontName, fontStyle, fontSize));
 		return captcha;
