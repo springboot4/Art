da8f70d64834bb91bb1211a79f7a1c1621198011
付绪壮 <2235602974@qq.com>
2023-01-22 14:43:16 +0800

    :sparkles: Introducing new features.art-spring-boot-starter-sensitive。

---
+++ art-framework/art-spring-boot-starter-sensitive/src/main/java/com/art/common/sensitive/core/handler/regex/EmailSensitiveHandler.java
@@ -0,0 +1,36 @@
+/*
+ * COPYRIGHT (C) 2023 Art AUTHORS(fxzcloud@gmail.com). ALL RIGHTS RESERVED.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.art.common.sensitive.core.handler.regex;
+
+import com.art.common.sensitive.core.annotation.regex.EmailSensitive;
+
+/**
+ * @author fxz
+ */
+public class EmailSensitiveHandler extends AbstractRegexSensitiveHandler<EmailSensitive> {
+
+	@Override
+	String getRegex(EmailSensitive annotation) {
+		return annotation.regex();
+	}
+
+	@Override
+	String getReplacer(EmailSensitive annotation) {
+		return annotation.replacer();
+	}
+
+}
