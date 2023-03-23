d7506cd376c276f1f065b1e369869ba77e919057
xuzhuang.fu 付绪壮 <2235602974@qq.com>
2023-03-23 19:29:33 +0800

    :sparkles: Introducing new features.support Hazelcast.

---
+++ art-framework/art-spring-boot-starter-hazelcast/src/main/java/com/art/common/hazelcast/core/base/DistributedQueueFactory.java
@@ -0,0 +1,34 @@
+/*
+ * COPYRIGHT (C) 2023 Art AUTHORS(fxzcloud@gmail.com). ALL RIGHTS RESERVED.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.art.common.hazelcast.core.base;
+
+/**
+ * @author Fxz
+ * @version 0.0.1
+ * @date 2023/3/23 17:14
+ */
+public interface DistributedQueueFactory {
+
+	/**
+	 * 获取分布式集合
+	 * @param name 分布式集合的名称
+	 * @param <T> 泛型
+	 * @return 分布式集合
+	 */
+	<T> DistributedQueue<T> getQueue(String name);
+
+}
--- art-framework/art-spring-boot-starter-hazelcast/src/main/java/com/art/common/hazelcast/core/support/HazelcastInstanceFactory.java
+++ art-framework/art-spring-boot-starter-hazelcast/src/main/java/com/art/common/hazelcast/core/support/HazelcastInstanceFactory.java
@@ -1,27 +1,53 @@
-package com.art.common.hazelcast.core;
+/*
+ * COPYRIGHT (C) 2023 Art AUTHORS(fxzcloud@gmail.com). ALL RIGHTS RESERVED.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.art.common.hazelcast.core.support;
 
 import com.hazelcast.config.Config;
 import com.hazelcast.config.NetworkConfig;
 import com.hazelcast.core.Hazelcast;
 import com.hazelcast.core.HazelcastInstance;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.FactoryBean;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 import java.util.StringJoiner;
 
 /**
  * HazelcastInstanceFactory 用于创建 HazelcastInstance
  *
  * @author fxz
+ * @version 0.0.1
+ * @date 2023/3/23 16:16
  */
 @RequiredArgsConstructor
-public class HazelcastInstanceFactory implements FactoryBean<HazelcastInstance> {
+public class HazelcastInstanceFactory implements FactoryBean<HazelcastInstance>, DisposableBean {
 
 	private final HazelcastProperties properties;
 
+	private HazelcastInstance instance;
+
 	public HazelcastInstance getInstance() {
+		if (Objects.nonNull(instance)) {
+			return instance;
+		}
+
 		// 配置实例名称
 		Config config = new Config(properties.getInstanceName());
 		// 配置集群名称
@@ -29,7 +55,9 @@ public class HazelcastInstanceFactory implements FactoryBean<HazelcastInstance> 
 		// 配置网络
 		configNetwork(config.getNetworkConfig());
 		// 创建实例
-		return Hazelcast.newHazelcastInstance(config);
+		instance = Hazelcast.newHazelcastInstance(config);
+
+		return instance;
 	}
 
 	private void configNetwork(NetworkConfig networkConfig) {
@@ -56,6 +84,11 @@ public class HazelcastInstanceFactory implements FactoryBean<HazelcastInstance> 
 	@Override
 	public Class<?> getObjectType() {
 		return HazelcastInstance.class;
+	}
+
+	@Override
+	public void destroy() throws Exception {
+		instance.shutdown();
 	}
 
 }
