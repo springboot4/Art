83188628a43aa9e89fbdfbc44f401f52ca65073a
xuzhuang.fu 付绪壮 <2235602974@qq.com>
2023-03-26 14:14:18 +0800

    :sparkles: Introducing new features.Support Hazelcast Client And Message Interceptor.

--- art-framework/art-spring-boot-starter-hazelcast/src/main/java/com/art/common/hazelcast/config/HazelcastClientAutoConfiguration.java
+++ art-framework/art-spring-boot-starter-hazelcast/src/main/java/com/art/common/hazelcast/config/HazelcastClientAutoConfiguration.java
@@ -1,42 +1,41 @@
-/// *
-// * COPYRIGHT (C) 2023 Art AUTHORS(fxzcloud@gmail.com). ALL RIGHTS RESERVED.
-// *
-// * Licensed under the Apache License, Version 2.0 (the "License");
-// * you may not use this file except in compliance with the License.
-// * You may obtain a copy of the License at
-// *
-// * http://www.apache.org/licenses/LICENSE-2.0
-// *
-// * Unless required by applicable law or agreed to in writing, software
-// * distributed under the License is distributed on an "AS IS" BASIS,
-// * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// * See the License for the specific language governing permissions and
-// * limitations under the License.
-// */
-//
-// package com.art.common.hazelcast.config;
-//
-// import com.art.common.hazelcast.core.support.HazelcastClientFactory;
-// import com.art.common.hazelcast.core.support.HazelcastProperties;
-// import com.art.common.hazelcast.core.support.HazelcastServerInstance;
-// import org.springframework.beans.factory.ObjectProvider;
-// import org.springframework.boot.autoconfigure.AutoConfiguration;
-//
-/// **
-// * @author Fxz
-// * @version 0.0.1
-// * @date 2023/3/26 09:43
-// */
-// @AutoConfiguration(after = HazelcastServerAutoConfiguration.class)
-// public class HazelcastClientAutoConfiguration {
-//
-// //@Bean
-// HazelcastClientFactory hazelcastInstance(HazelcastProperties properties,
-// ObjectProvider<HazelcastServerInstance> serverInstances) {
-// System.out.println("服务实例:" + serverInstances.getIfAvailable());
-// HazelcastClientFactory hazelcastClientFactory = new HazelcastClientFactory(properties);
-// System.out.println("客户端实例:" + hazelcastClientFactory);
-// return hazelcastClientFactory;
-// }
-//
-// }
+/*
+ * COPYRIGHT (C) 2023 Art AUTHORS(fxzcloud@gmail.com). ALL RIGHTS RESERVED.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.art.common.hazelcast.config;
+
+import com.art.common.hazelcast.core.support.HazelcastClientFactory;
+import com.art.common.hazelcast.core.support.HazelcastProperties;
+import com.art.common.hazelcast.core.support.HazelcastServerInstance;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.context.annotation.Bean;
+
+/**
+ * @author Fxz
+ * @version 0.0.1
+ * @date 2023/3/26 09:44
+ */
+@AutoConfiguration(after = HazelcastServerAutoConfiguration.class)
+public class HazelcastClientAutoConfiguration {
+
+	@Bean
+	HazelcastClientFactory hazelcastInstance(HazelcastProperties properties,
+			ObjectProvider<HazelcastServerInstance> serverInstances) {
+		serverInstances.getIfAvailable();
+		return new HazelcastClientFactory(properties);
+	}
+
+}
