32909a4fc78f63f3996cbaa194898835e0d1f8d2
xuzhuang.fu 付绪壮 <2235602974@qq.com>
2023-03-27 09:20:29 +0800

    :recycle: Refactor code.

--- art-framework/art-spring-boot-starter-hazelcast/src/main/java/com/art/common/hazelcast/core/base/DistributedBaseFactory.java
+++ art-framework/art-spring-boot-starter-hazelcast/src/main/java/com/art/common/hazelcast/core/base/DistributedBaseFactory.java
@@ -16,12 +16,14 @@
 
 package com.art.common.hazelcast.core.base;
 
+import java.util.concurrent.locks.Lock;
+
 /**
  * @author Fxz
  * @version 0.0.1
- * @date 2023/3/23 17:14
+ * @date 2023/3/27 09:10
  */
-public interface DistributedCountDownLatchFactory {
+public interface DistributedBaseFactory {
 
 	/**
 	 * 获取分布式集合
@@ -29,5 +31,37 @@ public interface DistributedCountDownLatchFactory {
 	 * @return 分布式集合
 	 */
 	DistributedCountDownLatch getCountDownLatch(String name);
+
+	/**
+	 * 获取分布式集合
+	 * @param name 分布式集合的名称
+	 * @return 分布式集合
+	 */
+	Lock getLock(String name);
+
+	/**
+	 * 获取分布式集合
+	 * @param name 分布式集合的名称
+	 * @param <K> 泛型
+	 * @param <V> 泛型
+	 * @return 分布式集合
+	 */
+	<K, V> DistributedMap<K, V> getMap(String name);
+
+	/**
+	 * 获取分布式集合
+	 * @param name 分布式集合的名称
+	 * @param <T> 泛型
+	 * @return 分布式集合
+	 */
+	<T> DistributedQueue<T> getQueue(String name);
+
+	/**
+	 * 获取分布式集合
+	 * @param name 分布式集合的名称
+	 * @param <T> 泛型
+	 * @return 分布式集合
+	 */
+	<T> DistributedSet<T> getSet(String name);
 
 }
