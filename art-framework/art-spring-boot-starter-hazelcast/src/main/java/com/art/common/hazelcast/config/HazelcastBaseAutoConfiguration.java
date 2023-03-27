4838ac3a5925b05988411e6b3b58266b6fbae4cd
xuzhuang.fu 付绪壮 <2235602974@qq.com>
2023-03-27 09:20:29 +0800

    :recycle: Refactor code.

--- art-framework/art-spring-boot-starter-hazelcast/src/main/java/com/art/common/hazelcast/config/HazelcastBaseAutoConfiguration.java
+++ art-framework/art-spring-boot-starter-hazelcast/src/main/java/com/art/common/hazelcast/config/HazelcastBaseAutoConfiguration.java
@@ -1,18 +1,9 @@
 package com.art.common.hazelcast.config;
 
-import com.art.common.hazelcast.core.base.DistributedCountDownLatchFactory;
-import com.art.common.hazelcast.core.base.DistributedLockFactory;
-import com.art.common.hazelcast.core.base.DistributedMapFactory;
-import com.art.common.hazelcast.core.base.DistributedQueueFactory;
-import com.art.common.hazelcast.core.base.DistributedSetFactory;
-import com.art.common.hazelcast.core.support.HazelcastCountDownLatchFactory;
-import com.art.common.hazelcast.core.support.HazelcastLockFactory;
-import com.art.common.hazelcast.core.support.HazelcastMapFactory;
+import com.art.common.hazelcast.core.base.DistributedBaseFactory;
+import com.art.common.hazelcast.core.support.HazelcastBaseFactory;
 import com.art.common.hazelcast.core.support.HazelcastProperties;
-import com.art.common.hazelcast.core.support.HazelcastQueueFactory;
-import com.art.common.hazelcast.core.support.HazelcastSetFactory;
 import com.hazelcast.core.HazelcastInstance;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
@@ -27,30 +18,8 @@ import org.springframework.context.annotation.Bean;
 public class HazelcastBaseAutoConfiguration {
 
 	@Bean
-	DistributedCountDownLatchFactory distributedCountDownLatchFactory(
-			@Qualifier("hazelcastInstance") HazelcastInstance hazelcastInstance) {
-		return new HazelcastCountDownLatchFactory(hazelcastInstance);
-	}
-
-	@Bean
-	DistributedLockFactory distributedLockFactory(@Qualifier("hazelcastInstance") HazelcastInstance hazelcastInstance) {
-		return new HazelcastLockFactory(hazelcastInstance);
-	}
-
-	@Bean
-	DistributedMapFactory distributedMapFactory(@Qualifier("hazelcastInstance") HazelcastInstance hazelcastInstance) {
-		return new HazelcastMapFactory(hazelcastInstance);
-	}
-
-	@Bean
-	DistributedQueueFactory distributedQueueFactory(
-			@Qualifier("hazelcastInstance") HazelcastInstance hazelcastInstance) {
-		return new HazelcastQueueFactory(hazelcastInstance);
-	}
-
-	@Bean
-	DistributedSetFactory distributedSetFactory(@Qualifier("hazelcastInstance") HazelcastInstance hazelcastInstance) {
-		return new HazelcastSetFactory(hazelcastInstance);
+	DistributedBaseFactory distributedBaseFactory(HazelcastInstance instance) {
+		return new HazelcastBaseFactory(instance);
 	}
 
 }
