705e11f0515ff8d8c7fe28bb47e0fabdb79915e6
付绪壮 <2235602974@qq.com>
2025-08-17 14:04:03 +0800

    :sparkles: 添加AI应用及工作流相关数据库表结构

---
+++ _other/docker/mysql8.0/docker-entrypoint-initdb.d/05-art_ai.sql
@@ -0,0 +1,66 @@
+
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for ai_app
+-- ----------------------------
+DROP TABLE IF EXISTS `ai_app`;
+CREATE TABLE `ai_app` (
+  `id` bigint NOT NULL,
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `description` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `mode` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `icon` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `status` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `create_time` datetime DEFAULT NULL,
+  `create_by` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `update_time` datetime DEFAULT NULL,
+  `update_by` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `tenant_id` bigint DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- ----------------------------
+-- Table structure for ai_workflow_runtime
+-- ----------------------------
+DROP TABLE IF EXISTS `ai_workflow_runtime`;
+CREATE TABLE `ai_workflow_runtime` (
+  `id` bigint DEFAULT NULL COMMENT '主键',
+  `app_id` bigint DEFAULT NULL COMMENT '应用id',
+  `workflow_id` bigint DEFAULT NULL COMMENT '工作流主键',
+  `input` json DEFAULT NULL COMMENT '流程入参',
+  `output` json DEFAULT NULL COMMENT '流程出参',
+  `status` int DEFAULT NULL COMMENT '执行状态',
+  `status_remark` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '状态描述',
+  `create_by` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `create_time` datetime DEFAULT NULL,
+  `update_time` datetime DEFAULT NULL,
+  `update_by` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `tenant_id` bigint DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- ----------------------------
+-- Table structure for ai_workflows
+-- ----------------------------
+DROP TABLE IF EXISTS `ai_workflows`;
+CREATE TABLE `ai_workflows` (
+  `id` bigint NOT NULL COMMENT '主键',
+  `app_id` bigint NOT NULL COMMENT '应用id',
+  `type` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '流程类型',
+  `version` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '流程版本',
+  `graph` json NOT NULL COMMENT '图信息',
+  `features` json DEFAULT NULL COMMENT '流程配置',
+  `environment_variables` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci COMMENT '环境变量',
+  `conversation_variables` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci COMMENT '会话变量',
+  `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+  `create_by` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '创建人',
+  `update_time` datetime DEFAULT NULL COMMENT '更新时间',
+  `update_by` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '更新人',
+  `tenant_id` bigint DEFAULT NULL COMMENT '租户id',
+  PRIMARY KEY (`id`) USING BTREE,
+  KEY `workflow_version_idx` (`app_id`,`version`,`tenant_id`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+SET FOREIGN_KEY_CHECKS = 1;
